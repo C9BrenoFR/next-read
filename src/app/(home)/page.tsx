76c87eefd03f475da9e386cfbb637857c779a43e
BrenoFR <breno.rosado@estudante.ufjf.com.br>
2024-08-13 14:14:00 -0300

    Troca da pasta da landing page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import Carousel from "@/components/landing_page/carousel";
+import HeroSection from "@/components/landing_page/hero_section";
+
+export default function Home() {
+  return (
+    <div>
+      <HeroSection />
+      <Carousel />
+    </div>
+  );
+}
