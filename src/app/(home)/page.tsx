de6115c9370ebf62bff77ca1f4b452324fea8e80
BrenoFR <breno.rosado@estudante.ufjf.com.br>
2024-08-15 21:55:41 -0300

    Landing page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,13 @@
 import Carousel from "@/components/landing_page/carousel";
 import HeroSection from "@/components/landing_page/hero_section";
+import Products from "@/components/landing_page/products";
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <Carousel />
+      <Products />
     </div>
   );
 }
