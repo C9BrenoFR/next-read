1ad10f63201f757bda51debe7fa3126b7b1677b2
BrenoFR <breno.rosado@estudante.ufjf.com.br>
2024-12-03 16:29:04 -0300

    Navabar com todos os links

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -6,8 +6,8 @@ import NavLink from "../nav_link";
 export default function Header() {
   return (
     <header className="bg-customWhite flex justify-center h-14"><div className="w-1/3 flex items-center justify-between h-full">
-        <NavLink href="/" originalText={<span className="material-symbols-outlined">store</span>} hoverContent={<p>Loja</p>} />
-        <NavLink href="/" originalText={<span className="material-symbols-outlined">call</span>} hoverContent={<p>Contato</p>} />
+        <NavLink href="/store" originalText={<span className="material-symbols-outlined">store</span>} hoverContent={<p>Loja</p>} />
+        <NavLink href="/call" originalText={<span className="material-symbols-outlined">call</span>} hoverContent={<p>Contato</p>} />
         <Link
           href="/"
           className="bg-[#011126] text-[#0cdbf2] h-full flex items-center justify-center px-4 rounded-b-2xl font-cinzel transition-transform duration-300 hover:neon-effect hover:scale-110"
@@ -15,7 +15,7 @@ export default function Header() {
           Next Read
         </Link>
         <NavLink href="/login" originalText={<span className="material-symbols-outlined">account_circle</span>} hoverContent={<p>Login</p>} />
-        <NavLink href="/" originalText={<span className="material-symbols-outlined">settings</span>} hoverContent={<p>Gerenciamento</p>} />
+        <NavLink href="/settings" originalText={<span className="material-symbols-outlined">settings</span>} hoverContent={<p>Gerenciamento</p>} />
       </div>
     </header>
   );
