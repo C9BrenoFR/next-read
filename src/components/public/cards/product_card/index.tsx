5e9496fd6fec0f4f5db743777e8e07700b5694a8
C9BrenoFR <breno.rosado@estudante.ufjf.br>
2025-01-14 20:15:21 -0300

    Faz com que o product card seja um link

--- src/components/public/cards/product_card/index.tsx
+++ src/components/public/cards/product_card/index.tsx
@@ -2,6 +2,7 @@ import { isEven } from "@/app/js/isEven";
 import Book from "@/components/public/book";
 import Image from "next/image";
 import { Product } from "@/interfaces/product";
+import Link from "next/link";
 
 interface ProductCardProps {
     width : string,
@@ -16,7 +17,7 @@ const odd = "neon-effect-2";
 
 export default function ProductCard({width, product, heigth, cardId, hover}: ProductCardProps) {
     return (
-        <article className={` ${width} ${heigth} p-3 pl-24 `}>
+        <Link href={`/store/${product.id}`} className={` ${width} ${heigth} p-3 pl-24 `}>
             <div className={` relative w-full h-full border-2 flex justify-end rounded rounded-se-3xl ${isEven(cardId) ? even : odd} ${hover ? 'ease-in-out transition-transform duration-300 hover:scale-110' : ''}`}>
                 <div className="absolute w-3/4 h-3/4 left-1 top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
                     {hover ? (
@@ -35,6 +36,6 @@ export default function ProductCard({width, product, heigth, cardId, hover}: Pro
                     <p className="text-xs p-2 mt-2">{product.description}</p>
                 </div>
             </div>
-        </article>
+        </Link>
     )
 }
