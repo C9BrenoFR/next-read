e4575eec87ac3aa2c23bbdb8e3a82ce53fd3fbe0
C9BrenoFR <breno.rosado@estudante.ufjf.br>
2025-01-14 15:43:23 -0300

    Função para buscar um produto com base no id

---
+++ src/backend/product/actions.ts
@@ -0,0 +1,13 @@
+import prisma from "@/lib/database";
+
+export async function getProduct(id:string) {
+
+    
+    const product = await prisma.book.findFirstOrThrow({
+        where: {
+            id: Number(id),
+        },
+    });
+
+    return product
+}
