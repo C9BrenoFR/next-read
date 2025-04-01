d8a488f4e690a561138a741e70fdefb42af69d5e
C9BrenoFR <breno.rosado@estudante.ufjf.br>
2025-03-31 23:20:52 -0300

    Trocando método domains por remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['via.placeholder.com', 'm.media-amazon.com'],
+      remotePatterns: [
+            {
+              protocol: "https",
+              hostname: "**", 
+            },
+          ],
     },
   };
   
